542102119f8eaa555c3d3842d68f0fc66c7f8af0
Junior Henrique <juniorhenrique030@outlook.com>
2020-07-22 19:21:30 -0300

    Criado classes enum, e get e setres, e teste de impressoes

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,10 +1,18 @@
 package Application;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {		
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		
+		System.out.println(order);
 	}
 
 }
